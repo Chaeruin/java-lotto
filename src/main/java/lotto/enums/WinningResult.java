f94e7a1214b364a0e5ed1297bdd823a5fc7dfc52
Chaeruin <cms3604@naver.com>
2024-11-21 12:18:46 +0900

    feat: enums 작성 및 기초 구조 작성

---
+++ src/main/java/lotto/enums/WinningResult.java
@@ -0,0 +1,44 @@
+package lotto.enums;
+
+public enum WinningResult {
+
+    THREE_WINNING(3, 5_000, 0, false),
+    FOUR_WINNING(4, 50_000, 0, false),
+    FIVE_WINNING(5, 1_500_000, 0, false),
+    FIVE_BONUS_WINNING(5, 30_000_000, 0 , true),
+    SIX_BONUS_WINNING(6, 2_000_000_000, 0, false);
+
+    private final int count;
+    private final int money;
+    private int winning;
+    private final boolean isBonus;
+
+    WinningResult(int count, int money, int winning, boolean isBonus) {
+        this.count = count;
+        this.money = money;
+        this.winning = winning;
+        this.isBonus = isBonus;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public int getMoney() {
+        return this.money;
+    }
+
+    public int getWinning() {
+        return this.winning;
+    }
+
+    public boolean getIsBonus() {
+        return this.isBonus;
+    }
+
+    public void setWinning(int winning) {
+        this.winning = winning;
+    }
+
+
+}
